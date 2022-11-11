a46141a4b86c25936cef33540124aad79f6ca078
solawing <316786359@qq.com>
2022-11-11 20:09:23 +0800

    init

---
+++ LinkMapAnalyzer/LinkMapAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkMapAnalyzerApp.swift
+//  LinkMapAnalyzer
+//
+//  Created by SolaWing on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct LinkMapAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
